31633b745951317719d2a2ff220f2a573a709b0a
51525 <lei.xiong@innodealing.com>
2022-06-13 23:10:16 +0800

    增加多音字测试类

--- src/test/java/org/nlpcn/commons/lang/TestUtils.java
+++ src/test/java/org/nlpcn/commons/lang/TestUtils.java
@@ -6,22 +6,24 @@ import org.nlpcn.commons.lang.pinyin.Pinyin;
 
 public class TestUtils {
 
-	public static String mainResources(final String file) {
-		return System.getProperties().getProperty("user.dir") + "/src/main/resources"
-				+ (file.startsWith("/") ? file : "/" + file);
-	}
+    public static String mainResources(final String file) {
+        return System.getProperties().getProperty("user.dir") + "/src/main/resources"
+                + (file.startsWith("/") ? file : "/" + file);
+    }
 
-	public static String testResources(final String file) {
-		return System.getProperties().getProperty("user.dir") + "/src/test/resources"
-				+ (file.startsWith("/") ? file : "/" + file);
-	}
+    public static String testResources(final String file) {
+        return System.getProperties().getProperty("user.dir") + "/src/test/resources"
+                + (file.startsWith("/") ? file : "/" + file);
+    }
 
-	public static void main(String[] args) {
-		List<String> parseStr = Pinyin.unicodePinyin("日往月来");
-		System.out.println(parseStr);
-		parseStr = Pinyin.pinyin("日往月来");
-		System.out.println(parseStr);
-		parseStr = Pinyin.tonePinyin("日往月来");
-		System.out.println(parseStr);
-	}
+    public static void main(String[] args) {
+        List<String> parseStr = Pinyin.multiplePinyin("行情");
+        System.out.println(parseStr);
+        parseStr = Pinyin.multipleFirstChar("行情");
+        System.out.println(parseStr);
+        parseStr = Pinyin.multipleUnicodePinyin("行情");
+        System.out.println(parseStr);
+        parseStr = Pinyin.multipleTonePinyin("行情");
+        System.out.println(parseStr);
+    }
 }
